64b0b594827c354d3667ce4c3c7ef4a388b8d67e
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-13 12:14:01 +0100

    config: confugere et installe envirenment with spring inisilizer

---
+++ src/main/java/org/sanaa/youcode/redline/unirent/UniRentApplication.java
@@ -0,0 +1,13 @@
+package org.sanaa.youcode.redline.unirent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sanaa/youcode/redline/unirent/UniRentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sanaa.youcode.redline.unirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
