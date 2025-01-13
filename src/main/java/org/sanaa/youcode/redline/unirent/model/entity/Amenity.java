63e1777a2719144ceeec67f639d5c272c4486898
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-13 15:22:58 +0100

    amenity property

--- src/main/java/org/sanaa/youcode/redline/unirent/model/entity/Amenity.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/entity/Amenity.java
@@ -1,4 +1,16 @@
 package org.sanaa.youcode.redline.unirent.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "amenities")
+@Data
 public class Amenity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/model/entity/AmenityProperty.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/entity/AmenityProperty.java
@@ -1,4 +1,20 @@
 package org.sanaa.youcode.redline.unirent.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "property_amenities")
+@Data
 public class AmenityProperty {
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    @ManyToOne
+    @JoinColumn(name = "amenity_id")
+    private Amenity amenity;
