d8c4cac447a02981950bb38ad56e728fbfff007e
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-13 15:18:37 +0100

    entities: add user and role tables

--- src/main/java/org/sanaa/youcode/redline/unirent/model/entity/Role.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/entity/Role.java
@@ -1,4 +1,24 @@
 package org.sanaa.youcode.redline.unirent.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Data
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String roleName;
+    private String description;
+
+    @OneToMany(mappedBy = "role")
+    private  List<User> user ;
+
+
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/model/entity/User.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/entity/User.java
@@ -1,4 +1,24 @@
 package org.sanaa.youcode.redline.unirent.model.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+
 }
