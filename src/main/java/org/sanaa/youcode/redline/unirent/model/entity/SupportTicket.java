08e71550d141e4232a75e72efe578fe29ce4e77f
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-13 15:31:44 +0100

    entity: support ticket for reclaim

--- src/main/java/org/sanaa/youcode/redline/unirent/model/entity/SupportTicket.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/entity/SupportTicket.java
@@ -1,4 +1,27 @@
 package org.sanaa.youcode.redline.unirent.model.entity;
 
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "support_tickets")
+@Data
 public class SupportTicket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long userId;
+    private String subject;
+    private String message;
+    private String status; // Open, Resolved
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private User user;
+
+
 }
+
