0bcc7af3688e20cb6bf4e0e68b9c2c10ca45ffa5
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-13 15:21:14 +0100

    entities: property table

--- src/main/java/org/sanaa/youcode/redline/unirent/model/entity/Property.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/entity/Property.java
@@ -1,4 +1,37 @@
 package org.sanaa.youcode.redline.unirent.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "properties")
+@Data
 public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String address;
+    private double price;
+    private boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "landlord_id")
+    private User landlord;
+
+    @ManyToOne
+    @JoinColumn(name = "university_id")
+    private University university;
+
+    @ManyToMany
+    @JoinTable(
+        name = "property_amenities",
+        joinColumns = @JoinColumn(name = "property_id"),
+        inverseJoinColumns = @JoinColumn(name = "amenity_id"))
+    private Set<Amenity> amenities;
+
+
 }
