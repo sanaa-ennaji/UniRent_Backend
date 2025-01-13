26ba66e527d29e1dfb3c0140e509e12870f67dfb
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-13 15:33:39 +0100

    table university

--- src/main/java/org/sanaa/youcode/redline/unirent/model/entity/University.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/entity/University.java
@@ -1,4 +1,30 @@
 package org.sanaa.youcode.redline.unirent.model.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "universities")
+@Data
 public class University {
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String location;
+    private String contactInfo;
+
+    @OneToMany(mappedBy = "university")
+    private List<Property> properties;
+
+
+
 }
+
