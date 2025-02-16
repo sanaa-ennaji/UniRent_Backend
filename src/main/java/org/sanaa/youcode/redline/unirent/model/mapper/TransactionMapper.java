3d14e13690aea6b590792395e4215c89c03f783d
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:17:51 +0100

    update on  22:17:51,85

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/TransactionMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/TransactionMapper.java
@@ -4,15 +4,15 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.sanaa.youcode.redline.unirent.model.dto.Request.TransactionRequestDTO;
 import org.sanaa.youcode.redline.unirent.model.dto.Response.TransactionResponseDTO;
-import org.sanaa.youcode.redline.unirent.model.entity.SupportTicket;
+import org.sanaa.youcode.redline.unirent.model.entity.Transaction;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring" )
 public interface TransactionMapper {
-    TransactionResponseDTO toResponseDTO(SupportTicket entity) ;
+    TransactionResponseDTO toResponseDTO(Transaction entity) ;
     SupportTicket toEntity(TransactionRequestDTO requestDTO);
-    List<TransactionResponseDTO> toResponseDTOList (List<SupportTicket> entities);
+    List<TransactionResponseDTO> toResponseDTOList (List<> entities);
     List<SupportTicket> toEntityList(List<TransactionRequestDTO> requestDTOs);
     void updateEntityFromRequest(TransactionRequestDTO amenityRequestDTO,@MappingTarget SupportTicket supportTicket);
 }
