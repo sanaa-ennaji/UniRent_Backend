936ca2eee8356722dd34040c93add7a56c83191a
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 21:56:45 +0100

    update on  21:56:45,61

--- src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/UserServiceI.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/UserServiceI.java
@@ -1,4 +1,14 @@
 package org.sanaa.youcode.redline.unirent.service.ServiceI;
 
+import org.sanaa.youcode.redline.unirent.model.dto.Request.UserRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.UserResponseDTO;
+
+import java.util.List;
+
 public interface UserServiceI {
+    UserResponseDTO getUserById(Long id);
+    List<UserResponseDTO> getAllUsers();
+    UserResponseDTO createUser(UserRequestDTO requestDTO);
+    UserResponseDTO updateUser(Long id, UserRequestDTO requestDTO);
+    void deleteUser(Long id);
 }
