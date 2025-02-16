8de796530bfb1299b8d6fa1119af3bc72e9a07e2
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 21:52:05 +0100

    update on  21:52:05,79

--- src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/PropertyServiceI.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/PropertyServiceI.java
@@ -1,4 +1,14 @@
 package org.sanaa.youcode.redline.unirent.service.ServiceI;
 
+import org.sanaa.youcode.redline.unirent.model.dto.Request.PropertyRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.PropertyResponseDTO;
+
+import java.util.List;
+
 public interface PropertyServiceI {
+    PropertyResponseDTO getPropertyById(Long id);
+    List<PropertyResponseDTO> getAllProperties();
+    PropertyResponseDTO createProperty(PropertyRequestDTO requestDTO);
+    PropertyResponseDTO updateProperty(Long id, PropertyRequestDTO requestDTO);
+    void deleteProperty(Long id);
 }
