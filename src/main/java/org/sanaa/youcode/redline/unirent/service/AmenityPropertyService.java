5203bb07cba0935b902742ef05c304dc7f9f98bf
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:55:04 +0100

    update on  22:55:04,60

--- src/main/java/org/sanaa/youcode/redline/unirent/service/AmenityPropertyService.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/AmenityPropertyService.java
@@ -1,11 +1,54 @@
 package org.sanaa.youcode.redline.unirent.service;
 
 import jakarta.transaction.Transactional;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.AmenityPropertyResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.entity.AmenityProperty;
+import org.sanaa.youcode.redline.unirent.repository.AmenityPropertyRepository;
+import org.sanaa.youcode.redline.unirent.service.ServiceI.AmenityPropertyServiceI;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
-public class AmenityPropertyService {
+public class AmenityPropertyService  implements AmenityPropertyServiceI {
+    private final AmenityPropertyRepository amenityPropertyRepository;
+    private final AmenityPropertyMapper amenityPropertyMapper;
 
+    public AmenityPropertyService(AmenityPropertyRepository amenityPropertyRepository, AmenityPropertyMapper amenityPropertyMapper) {
+        this.amenityPropertyRepository = amenityPropertyRepository;
+        this.amenityPropertyMapper = amenityPropertyMapper;
+    }
+
+    @Override
+    public AmenityPropertyResponseDTO getAmenityPropertyById(Long id) {
+        AmenityProperty amenityProperty = amenityPropertyRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("AmenityProperty not found"));
+        return amenityPropertyMapper.toResponseDTO(amenityProperty);
+    }
+
+    @Override
+    public List<AmenityPropertyResponseDTO> getAllAmenityProperties() {
+        return amenityPropertyMapper.toResponseDTOList(amenityPropertyRepository.findAll());
+    }
+
+    @Override
+    public AmenityPropertyResponseDTO createAmenityProperty(AmenityPropertyRequestDTO requestDTO) {
+        AmenityProperty amenityProperty = amenityPropertyMapper.toEntity(requestDTO);
+        return amenityPropertyMapper.toResponseDTO(amenityPropertyRepository.save(amenityProperty));
+    }
+
+    @Override
+    public AmenityPropertyResponseDTO updateAmenityProperty(Long id, AmenityPropertyRequestDTO requestDTO) {
+        AmenityProperty amenityProperty = amenityPropertyRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("AmenityProperty not found"));
+        amenityPropertyMapper.updateEntityFromRequest(requestDTO, amenityProperty);
+        return amenityPropertyMapper.toResponseDTO(amenityPropertyRepository.save(amenityProperty));
+    }
+
+    @Override
+    public void deleteAmenityProperty(Long id) {
+        amenityPropertyRepository.deleteById(id);
+    }
 
 }
