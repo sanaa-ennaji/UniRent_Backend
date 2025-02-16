ac4fa1ff7ca3e7c4a5c910c5ef96a0421e02d088
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 20:41:23 +0100

    update on  20:41:23,31

--- src/main/java/org/sanaa/youcode/redline/unirent/repository/UserRepository.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package org.sanaa.youcode.redline.unirent.repository;
 
-public class UserRepository {
+import org.sanaa.youcode.redline.unirent.model.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
 }
