4465e152719a8559272005d0210181b0a135377b
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 16:41:14 +0100

    update on  16:41:14,63

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/ImageRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/ImageRequestDTO.java
@@ -1,4 +1,12 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class ImageRequestDTO {
+    @NotBlank
+    private String imageUrl;
+
+    @NotNull
+    private Long propertyId;
 }
