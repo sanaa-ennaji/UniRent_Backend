a83a3b391e32296c2da129db81cdab3ffc3adc24
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 16:43:00 +0100

    update on  16:43:00,10

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/SupportTicketReqestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/SupportTicketReqestDTO.java
@@ -1,4 +1,22 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SupportTicketReqestDTO {
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String message;
+
+    @NotNull
+    private Long userId;
 }
