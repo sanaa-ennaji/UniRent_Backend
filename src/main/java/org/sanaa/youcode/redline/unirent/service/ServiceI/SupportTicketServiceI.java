0930dfc4b758f172ffbdfe51c6332ca5261688a4
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:34:20 +0100

    update on  22:34:20,05

--- src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/SupportTicketServiceI.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/SupportTicketServiceI.java
@@ -1,4 +1,14 @@
 package org.sanaa.youcode.redline.unirent.service.ServiceI;
 
+import org.sanaa.youcode.redline.unirent.model.dto.Request.SupportTicketRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.SupportTicketResponseDTO;
+
+import java.util.List;
+
 public interface SupportTicketServiceI {
+    SupportTicketResponseDTO getTicketById(Long id);
+    List<SupportTicketResponseDTO> getAllTickets();
+    SupportTicketResponseDTO createTicket(SupportTicketRequestDTO requestDTO);
+    SupportTicketResponseDTO updateTicket(Long id, SupportTicketRequestDTO requestDTO);
+    void deleteTicket(Long id);
 }
