0630af30937ad17c9c26b8edf867245e25965e56
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 17:12:06 +0100

    update on  17:12:06,58

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/PropertyResponseDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/PropertyResponseDTO.java
@@ -1,4 +1,23 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PropertyResponseDTO {
+    private Long id;
+    private String title;
+    private String address;
+    private double price;
+    private boolean available;
+    private Long landlordId;
+    private String landlordName;
+    private Long universityId;
+    private String universityName;
+    private List<String> imageUrls;
 }
