e85046f849452f0a94bae94aa35a02a0c64e4e6c
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 16:15:43 +0100

    update on  16:15:43,85

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/RoleRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/RoleRequestDTO.java
@@ -1,4 +1,16 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoleRequestDTO {
+    @NotBlank
+    private String roleName;
+
+    private String description;
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/UserRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/UserRequestDTO.java
@@ -1,4 +1,13 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequestDTO {
+
 }
