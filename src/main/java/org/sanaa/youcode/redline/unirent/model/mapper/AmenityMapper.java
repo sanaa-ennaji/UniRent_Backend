64c5b3643d082cecab3bb81cf67e78262901035e
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 17:30:21 +0100

    update on  17:30:21,13

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/AmenityMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/AmenityMapper.java
@@ -1,5 +1,5 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
 public interface AmenityMapper {
-    
+
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/UserMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
+import org.sanaa.youcode.redline.unirent.model.entity.AppUser;
+import org.springframework.web.bind.annotation.Mapping;
+
 public interface UserMapper {
+
+    @Mapping(source = "role.id", target = "roleId")
+    @Mapping(source = "role.roleName", target = "roleName")
+    AppUserResponseDTO toDto(AppUser appUser);
+
+    @Mapping(source = "roleId", target = "role.id")
+    AppUser toEntity(AppUserRequestDTO dto);
 }
