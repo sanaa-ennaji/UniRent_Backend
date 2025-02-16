031b57d27b30b15813a560765131ee8d4acb059d
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:53:29 +0100

    update on  22:53:29,84

--- src/main/java/org/sanaa/youcode/redline/unirent/service/AmenityPropertyService.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/AmenityPropertyService.java
@@ -1,8 +1,10 @@
 package org.sanaa.youcode.redline.unirent.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class AmenityPropertyService {
 
 
