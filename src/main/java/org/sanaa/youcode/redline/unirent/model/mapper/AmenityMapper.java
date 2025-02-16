da7c99a8a3eb80f7133d3252f687029c06e1fbc8
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 18:11:35 +0100

    update on  18:11:35,66

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/AmenityMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/AmenityMapper.java
@@ -1,5 +1,20 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.AmenityRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.AmenityResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.entity.Amenity;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring" )
 public interface AmenityMapper {
+
+    AmenityResponseDTO toResponseDTO(Amenity entity) ;
+    Amenity toEntity(AmenityRequestDTO requestDTO);
+    List<AmenityResponseDTO> toResponseDTOList (List<Amenity> entities);
+    List<Amenity> toEntityList(List<AmenityRequestDTO> requestDTOs);
+    void updateEntityFromRequest(AmenityRequestDTO amenityRequestDTO,@MappingTarget Amenity amenity);
 
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/AmenityProperty.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/AmenityProperty.java
@@ -1,4 +1,13 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
 public interface AmenityProperty {
+    ResponseDTO toResponseDTO(Amenity entity) ;
+    Amenity toEntity(AmenityRequestDTO requestDTO);
+    List<AmenityResponseDTO> toResponseDTOList (List<Amenity> entities);
+    List<Amenity> toEntityList(List<AmenityRequestDTO> requestDTOs);
+    void updateEntityFromRequest(AmenityRequestDTO amenityRequestDTO,@MappingTarget Amenity amenity);
 }
