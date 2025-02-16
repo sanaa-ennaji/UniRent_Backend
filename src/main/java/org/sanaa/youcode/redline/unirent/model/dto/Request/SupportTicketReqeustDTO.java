560e1dfa1d5c253b44b6af073a790d27753967c5
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 18:35:10 +0100

    update on  18:35:09,91

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/SupportTicketReqeustDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/SupportTicketReqeustDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class SupportTicketReqestDTO {
+public class SupportTicketReqeustDTO {
     @NotBlank
     private String subject;
 
