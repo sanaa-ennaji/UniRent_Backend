32f423b0e36965c9f79c9428d6a7505bb4143d04
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 14:13:17 +0100

    update on  14:13:17,86

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/AmenityPropertyRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/AmenityPropertyRequestDTO.java
@@ -1,5 +1,7 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AmenityPropertyRequestDTO {
+    @NotNull
+    @Min(0)
+    private int quantity;
 
+    @NotNull
+    private Long propertyId;
+
+    @NotNull
+    private Long amenityId;
 }
