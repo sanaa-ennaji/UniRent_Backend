db6f9924c0d3ea096bdab1740682d084e4535dc6
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 17:02:40 +0100

    update on  17:02:39,98

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/RoleResponseDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/RoleResponseDTO.java
@@ -1,5 +1,13 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoleResponseDTO {
     private Long id;
     private String roleName;
