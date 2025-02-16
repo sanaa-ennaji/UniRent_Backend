627ea077713ab27d18fc8ac1093b0ea5e78e4116
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:35:16 +0100

    update on  22:35:16,89

--- src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/RoleServiceI.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/RoleServiceI.java
@@ -1,5 +1,14 @@
 package org.sanaa.youcode.redline.unirent.service.ServiceI;
 
+import org.sanaa.youcode.redline.unirent.model.dto.Request.RoleRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.RoleResponseDTO;
+
+import java.util.List;
+
 public interface RoleServiceI {
-    
+    RoleResponseDTO getRoleById(Long id);
+    List<RoleResponseDTO> getAllRoles();
+    RoleResponseDTO createRole(RoleRequestDTO requestDTO);
+    RoleResponseDTO updateRole(Long id, RoleRequestDTO requestDTO);
+    void deleteRole(Long id);
 }
