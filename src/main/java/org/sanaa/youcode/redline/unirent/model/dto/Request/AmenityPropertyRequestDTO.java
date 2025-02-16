1a240f3c92a47a09dc2c7eb590cbb9058cf05f49
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 13:57:14 +0100

    update on  13:57:14,48

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/AmenityPropertyRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/AmenityPropertyRequestDTO.java
@@ -1,4 +1,14 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AmenityPropertyRequestDTO {
+
+
 }
+
