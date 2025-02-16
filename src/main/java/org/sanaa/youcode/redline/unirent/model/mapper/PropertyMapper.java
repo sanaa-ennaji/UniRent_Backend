0a06676f3254e1498989549692331c3910dcf41b
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 18:27:24 +0100

    update on  18:27:24,72

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/PropertyMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/PropertyMapper.java
@@ -1,4 +1,19 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.ImageRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.PropertyRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.PropertyResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.entity.Image;
+import org.sanaa.youcode.redline.unirent.model.entity.Property;
+
+import java.util.List;
+@Mapper(componentModel = "spring" )
 public interface PropertyMapper {
+    PropertyResponseDTO toResponseDTO(Property entity) ;
+    Property toEntity(ImageRequestDTO requestDTO);
+    List<PropertyResponseDTO> toResponseDTOList (List<Image> entities);
+    List<Property> toEntityList(List<PropertyRequestDTO> requestDTOs);
+    void updateEntityFromRequest(PropertyRequestDTO propertyRequestDTO, @MappingTarget Property property);
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/ReviewMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/ReviewMapper.java
@@ -1,4 +1,19 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.ReviewRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.ReviewResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.entity.Review;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring" )
 public interface ReviewMapper {
+
+    ReviewResponseDTO toResponseDTO(Review entity) ;
+    Review toEntity(ReviewRequestDTO requestDTO);
+    List<ReviewResponseDTO> toResponseDTOList (List<Review> entities);
+    List<Review> toEntityList(List<ReviewRequestDTO> requestDTOs);
+    void updateEntityFromRequest(ReviewRequestDTO amenityRequestDTO,@MappingTarget Review review);
 }
