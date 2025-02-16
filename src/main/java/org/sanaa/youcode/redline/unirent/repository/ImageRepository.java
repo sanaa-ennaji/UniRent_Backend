53d110fb30210328c8d7b0924a101dfd5f8e2cba
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 20:35:14 +0100

    update on  20:35:14,69

--- src/main/java/org/sanaa/youcode/redline/unirent/repository/ImageRepository.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/repository/ImageRepository.java
@@ -1,4 +1,9 @@
 package org.sanaa.youcode.redline.unirent.repository;
 
-public class ImageRepository {
+import org.sanaa.youcode.redline.unirent.model.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
 }
