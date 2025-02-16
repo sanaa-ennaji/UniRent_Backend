f913f5bd45d4344fee33795a56d45b79da18a22a
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 20:34:44 +0100

    update on  20:34:44,25

--- src/main/java/org/sanaa/youcode/redline/unirent/repository/BookingRepository.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/repository/BookingRepository.java
@@ -1,4 +1,9 @@
 package org.sanaa.youcode.redline.unirent.repository;
 
-public class BookingRepository {
+import org.sanaa.youcode.redline.unirent.model.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
