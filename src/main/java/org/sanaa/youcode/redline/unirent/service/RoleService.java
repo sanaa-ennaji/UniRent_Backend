5cb4944a3c1690ddb862f0f71aab150971b487e3
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:38:19 +0100

    update on  22:38:19,88

--- src/main/java/org/sanaa/youcode/redline/unirent/service/RoleService.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/RoleService.java
@@ -1,4 +1,55 @@
 package org.sanaa.youcode.redline.unirent.service;
 
-public class RoleService {
+import jakarta.transaction.Transactional;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.RoleRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.RoleResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.mapper.RoleMapper;
+import org.sanaa.youcode.redline.unirent.repository.RoleRepository;
+import org.sanaa.youcode.redline.unirent.service.ServiceI.RoleServiceI;
+import org.springframework.stereotype.Service;
+
+import javax.management.relation.Role;
+import java.util.List;
+
+@Service
+@Transactional
+public class RoleService implements RoleServiceI {
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+
+    public RoleService(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
+    }
+
+    @Override
+    public RoleResponseDTO getRoleById(Long id) {
+        Role role = roleRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Role not found"));
+        return roleMapper.toResponseDTO(role);
+    }
+
+    @Override
+    public List<RoleResponseDTO> getAllRoles() {
+        return roleMapper.toResponseDTOList(roleRepository.findAll());
+    }
+
+    @Override
+    public RoleResponseDTO createRole(RoleRequestDTO requestDTO) {
+        Role role = roleMapper.toEntity(requestDTO);
+        return roleMapper.toResponseDTO(roleRepository.save(role));
+    }
+
+    @Override
+    public RoleResponseDTO updateRole(Long id, RoleRequestDTO requestDTO) {
+        Role role = roleRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Role not found"));
+        roleMapper.updateEntityFromRequest(requestDTO, role);
+        return roleMapper.toResponseDTO(roleRepository.save(role));
+    }
+
+    @Override
+    public void deleteRole(Long id) {
+        roleRepository.deleteById(id);
+    }
 }
