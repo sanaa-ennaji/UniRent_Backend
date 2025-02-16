acf69d9de4cbd791709ba2255728fd8516e988f1
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 16:42:18 +0100

    update on  16:42:18,77

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/PropertyRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/PropertyRequestDTO.java
@@ -1,4 +1,29 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PropertyRequestDTO {
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String address;
+
+    @NotNull
+    private double price;
+
+    private boolean available;
+
+    @NotNull
+    private Long landlordId;
+
+    @NotNull
+    private Long universityId;
 }
