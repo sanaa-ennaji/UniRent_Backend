f159189b5e69918372224469844d16dd57257765
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 18:34:34 +0100

    update on  18:34:34,76

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/SupportTicketMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/SupportTicketMapper.java
@@ -1,4 +1,14 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.SupportTicketResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.entity.SupportTicket;
+
+@Mapper(componentModel = "spring" )
 public interface SupportTicketMapper {
+    SupportTicketResponseDTO toResponseDTO(SupportTicket entity) ;
+    SupportTicket toEntity(SupportTicketRequestDTO requestDTO);
+    List<ImageResponseDTO> toResponseDTOList (List<Image> entities);
+    List<Image> toEntityList(List<ImageRequestDTO> requestDTOs);
+    void updateEntityFromRequest(AmenityRequestDTO amenityRequestDTO,@MappingTarget Image image);
 }
