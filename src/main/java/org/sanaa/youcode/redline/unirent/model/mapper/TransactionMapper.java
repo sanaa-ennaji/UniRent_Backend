98a97a12e3a118c95a0adf3b5791ab958818d906
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:16:17 +0100

    update on  22:16:17,47

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/TransactionMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/TransactionMapper.java
@@ -2,17 +2,17 @@ package org.sanaa.youcode.redline.unirent.model.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
-import org.sanaa.youcode.redline.unirent.model.dto.Request.SupportTicketRequestDTO;
-import org.sanaa.youcode.redline.unirent.model.dto.Response.SupportTicketResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.TransactionRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.TransactionResponseDTO;
 import org.sanaa.youcode.redline.unirent.model.entity.SupportTicket;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring" )
 public interface TransactionMapper {
-    SupportTicketResponseDTO toResponseDTO(SupportTicket entity) ;
-    SupportTicket toEntity(SupportTicketRequestDTO requestDTO);
-    List<SupportTicketResponseDTO> toResponseDTOList (List<SupportTicket> entities);
-    List<SupportTicket> toEntityList(List<SupportTicketRequestDTO> requestDTOs);
-    void updateEntityFromRequest(SupportTicketRequestDTO amenityRequestDTO,@MappingTarget SupportTicket supportTicket);
+    TransactionResponseDTO toResponseDTO(SupportTicket entity) ;
+    SupportTicket toEntity(TransactionRequestDTO requestDTO);
+    List<TransactionResponseDTO> toResponseDTOList (List<SupportTicket> entities);
+    List<SupportTicket> toEntityList(List<TransactionRequestDTO> requestDTOs);
+    void updateEntityFromRequest(TransactionRequestDTO amenityRequestDTO,@MappingTarget SupportTicket supportTicket);
 }
