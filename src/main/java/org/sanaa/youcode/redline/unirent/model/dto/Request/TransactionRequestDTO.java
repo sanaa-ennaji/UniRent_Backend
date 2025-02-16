2768980e8aa46741adf49d7305214d7a68eacf67
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 16:48:32 +0100

    update on  16:48:32,89

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/TransactionRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/TransactionRequestDTO.java
@@ -1,6 +1,26 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionRequestDTO {
 
-    
+    @Min(0)
+    private double amount;
+
+    @NotBlank
+    private String paymentMethod;
+
+    @NotBlank
+    private String paymentStatus;
+
+    @NotNull
+    private Long bookingId;
 }
