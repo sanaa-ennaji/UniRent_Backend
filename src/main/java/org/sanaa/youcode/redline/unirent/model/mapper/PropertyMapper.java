255811eb3219c742dac34f00c4f86560333d0c03
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 21:56:03 +0100

    update on  21:56:03,12

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/PropertyMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/PropertyMapper.java
@@ -2,18 +2,16 @@ package org.sanaa.youcode.redline.unirent.model.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
-import org.sanaa.youcode.redline.unirent.model.dto.Request.ImageRequestDTO;
 import org.sanaa.youcode.redline.unirent.model.dto.Request.PropertyRequestDTO;
 import org.sanaa.youcode.redline.unirent.model.dto.Response.PropertyResponseDTO;
-import org.sanaa.youcode.redline.unirent.model.entity.Image;
 import org.sanaa.youcode.redline.unirent.model.entity.Property;
 
 import java.util.List;
 @Mapper(componentModel = "spring" )
 public interface PropertyMapper {
     PropertyResponseDTO toResponseDTO(Property entity) ;
-    Property toEntity(ImageRequestDTO requestDTO);
-    List<PropertyResponseDTO> toResponseDTOList (List<Image> entities);
+    Property toEntity(PropertyRequestDTO requestDTO);
+    List<PropertyResponseDTO> toResponseDTOList (List<Property> entities);
     List<Property> toEntityList(List<PropertyRequestDTO> requestDTOs);
     void updateEntityFromRequest(PropertyRequestDTO propertyRequestDTO, @MappingTarget Property property);
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/service/PropertyService.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/PropertyService.java
@@ -1,4 +1,55 @@
 package org.sanaa.youcode.redline.unirent.service;
 
-public class PropertyService {
+import jakarta.transaction.Transactional;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.PropertyRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.PropertyResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.entity.Property;
+import org.sanaa.youcode.redline.unirent.model.mapper.PropertyMapper;
+import org.sanaa.youcode.redline.unirent.repository.PropertyRepository;
+import org.sanaa.youcode.redline.unirent.service.ServiceI.PropertyServiceI;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class PropertyService implements PropertyServiceI {
+    private final PropertyRepository propertyRepository;
+    private final PropertyMapper propertyMapper;
+
+    public PropertyService(PropertyRepository propertyRepository, PropertyMapper propertyMapper) {
+        this.propertyRepository = propertyRepository;
+        this.propertyMapper = propertyMapper;
+    }
+
+    @Override
+    public PropertyResponseDTO getPropertyById(Long id) {
+        Property property = propertyRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Property not found"));
+        return propertyMapper.toResponseDTO(property);
+    }
+
+    @Override
+    public List<PropertyResponseDTO> getAllProperties() {
+        return propertyMapper.toResponseDTOList(propertyRepository.findAll());
+    }
+
+    @Override
+    public PropertyResponseDTO createProperty(PropertyRequestDTO requestDTO) {
+        Property property = propertyMapper.toEntity(requestDTO);
+        return propertyMapper.toResponseDTO(propertyRepository.save(property));
+    }
+
+    @Override
+    public PropertyResponseDTO updateProperty(Long id, PropertyRequestDTO requestDTO) {
+        Property property = propertyRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Property not found"));
+        propertyMapper.updateEntityFromRequest(requestDTO, property);
+        return propertyMapper.toResponseDTO(propertyRepository.save(property));
+    }
+
+    @Override
+    public void deleteProperty(Long id) {
+        propertyRepository.deleteById(id);
+    }
 }
