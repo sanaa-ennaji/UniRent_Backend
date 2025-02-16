a28156833f14045063bcedf8f432d1cab5351b29
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 17:04:04 +0100

    update on  17:04:04,07

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/BookingResponseDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/BookingResponseDTO.java
@@ -1,4 +1,20 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingResponseDTO {
+    private Long id;
+    private String startDate;
+    private String endDate;
+    private String status;
+    private Long propertyId;
+    private String propertyTitle;
+    private Long studentId;
+    private String studentName;
 }
