82be7b99b5b0f2260b8710b528b4ed7293648e0f
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 23:10:53 +0100

    update on  23:10:53,47

--- src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/BookingServiceI.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/BookingServiceI.java
@@ -1,5 +1,14 @@
 package org.sanaa.youcode.redline.unirent.service.ServiceI;
 
+import org.sanaa.youcode.redline.unirent.model.dto.Request.BookingRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.BookingResponseDTO;
+
+import java.util.List;
+
 public interface BookingServiceI {
-    
+    BookingResponseDTO getBookingById(Long id);
+    List<BookingResponseDTO> getAllBookings();
+    BookingResponseDTO createBooking(BookingRequestDTO requestDTO);
+    BookingResponseDTO updateBooking(Long id, BookingRequestDTO requestDTO);
+    void deleteBooking(Long id);
 }
