376b0a98349220f5d97b66471ee3d1e96855cf57
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 17:12:55 +0100

    update on  17:12:55,45

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/SupportTicketResponseDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/SupportTicketResponseDTO.java
@@ -1,4 +1,19 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SupportTicketResponseDTO {
+
+    private Long id;
+    private String subject;
+    private String message;
+    private String status;
+    private Long userId;
+    private String userName;
 }
