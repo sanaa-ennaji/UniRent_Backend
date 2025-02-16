fc8a4631395a5068c0998de7b151e4c4f81278a2
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 20:33:34 +0100

    update on  20:33:34,19

--- src/main/java/org/sanaa/youcode/redline/unirent/repository/AmenityRepository.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/repository/AmenityRepository.java
@@ -1,4 +1,9 @@
 package org.sanaa.youcode.redline.unirent.repository;
 
-public class AmenityRepository {
+import org.sanaa.youcode.redline.unirent.model.entity.Amenity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AmenityRepository extends JpaRepository<Amenity, Long> {
 }
