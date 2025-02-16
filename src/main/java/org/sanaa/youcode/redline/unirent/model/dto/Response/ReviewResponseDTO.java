5a7f84375a4c2438e0b31ae70370adacd13c0d8f
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 17:23:17 +0100

    update on  17:23:17,81

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/ReviewResponseDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/ReviewResponseDTO.java
@@ -1,4 +1,18 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewResponseDTO {
+
+    private Long id;
+    private int rating;
+    private String comments;
+    private long propertyId;
+    private Long userId;
 }
