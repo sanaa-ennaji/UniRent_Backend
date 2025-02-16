280254106b211bfb611167a53e614b05d82b1321
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 16:39:58 +0100

    update on  16:39:58,81

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/BookingRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/BookingRequestDTO.java
@@ -1,5 +1,25 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingRequestDTO {
+    @NotBlank
+    private String startDate;
+
+    @NotBlank
+    private String endDate;
+
+    @NotNull
+    private Long propertyId;
+
+    @NotNull
+    private Long studentId;
 
 }
