974ab180486fd8dbf14ebc23b6aae0b30bc5063e
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 18:00:49 +0100

    update on  18:00:49,64

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/UserMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/UserMapper.java
@@ -1,13 +1,12 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
+import org.mapstruct.Mapper;
 import org.sanaa.youcode.redline.unirent.model.dto.Request.RoleRequestDTO;
 import org.sanaa.youcode.redline.unirent.model.dto.Response.RoleResponseDTO;
 
 import javax.management.relation.Role;
 @Mapper(componentModel = "spring" )
 public interface UserMapper {
-
     RoleResponseDTO toDto(Role role);
-
     Role toEntity(RoleRequestDTO dto);
 }
