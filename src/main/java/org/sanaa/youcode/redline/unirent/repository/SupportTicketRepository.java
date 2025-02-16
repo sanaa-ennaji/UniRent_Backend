0fba40f8004cfd262f03aaaf1b89486e16c1d5de
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 20:39:42 +0100

    update on  20:39:42,21

--- src/main/java/org/sanaa/youcode/redline/unirent/repository/SupportTicketRepository.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/repository/SupportTicketRepository.java
@@ -1,4 +1,9 @@
 package org.sanaa.youcode.redline.unirent.repository;
 
-public class SupportTicketRepository {
+import org.sanaa.youcode.redline.unirent.model.entity.SupportTicket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupportTicketRepository extends JpaRepository<SupportTicket, Long> {
 }
