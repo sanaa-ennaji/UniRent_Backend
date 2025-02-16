9c80f4846e31c27d49993a3d3ae3708e117f0fb9
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 17:13:48 +0100

    update on  17:13:48,45

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/TransactionResponseDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Response/TransactionResponseDTO.java
@@ -1,4 +1,17 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionResponseDTO {
+
+    private Long id;
+    private double amount;
+    private String paymentMethod;
+    private String paymentStatus;
+    private Long bookingId;
 }
