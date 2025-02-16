4da81eb277e7038e7dd29890887d6286fe905aaf
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:03:41 +0100

    update on  22:03:41,64

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/UserMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/UserMapper.java
@@ -1,10 +1,13 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.sanaa.youcode.redline.unirent.model.dto.Request.UserRequestDTO;
 import org.sanaa.youcode.redline.unirent.model.dto.Response.UserResponseDTO;
 import org.sanaa.youcode.redline.unirent.model.entity.AppUser;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring" )
 public interface UserMapper {
@@ -13,4 +16,7 @@ public interface UserMapper {
     UserResponseDTO toResponseDto(AppUser appUser);
     @Mapping(source = "roleId", target = "role.id")
     AppUser toEntity(UserRequestDTO dto);
+    List<UserResponseDTO> toResponseDTOList (List<AppUser> entities);
+    List<AppUser> toEntityList(List<UserRequestDTO> requestDTOs);
+    void updateEntityFromRequest(UserRequestDTO amenityRequestDTO,@MappingTarget AppUser appUser);
 }
