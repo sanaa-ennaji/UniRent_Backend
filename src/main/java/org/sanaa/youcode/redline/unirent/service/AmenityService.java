0aefc27448888eca805e0f487af59c3637cd3261
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 21:43:22 +0100

    update on  21:43:22,70

--- src/main/java/org/sanaa/youcode/redline/unirent/service/AmenityService.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/AmenityService.java
@@ -1,4 +1,8 @@
 package org.sanaa.youcode.redline.unirent.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AmenityService {
+
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/AmenityServiceI.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/AmenityServiceI.java
@@ -1,4 +1,9 @@
 package org.sanaa.youcode.redline.unirent.service.ServiceI;
 
 public interface AmenityServiceI {
+    AmenityResponseDTO getAmenityById(Long id);
+    List<AmenityResponseDTO> getAllAmenities();
+    AmenityResponseDTO createAmenity(AmenityRequestDTO requestDTO);
+    AmenityResponseDTO updateAmenity(Long id, AmenityRequestDTO requestDTO);
+    void deleteAmenity(Long id);
 }
