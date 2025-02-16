d12efd5a39200678f88cf667d8d4d8aef9167398
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 18:22:02 +0100

    update on  18:22:02,06

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/ImageMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/ImageMapper.java
@@ -1,4 +1,20 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.AmenityRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.ImageRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.ImageResponseDTO;
+import org.sanaa.youcode.redline.unirent.model.entity.Image;
+
+import java.util.List;
+
+
+@Mapper(componentModel = "spring" )
 public interface ImageMapper {
+    ImageResponseDTO toResponseDTO(Image entity) ;
+    Image toEntity(ImageRequestDTO requestDTO);
+    List<ImageResponseDTO> toResponseDTOList (List<Image> entities);
+    List<Image> toEntityList(List<ImageRequestDTO> requestDTOs);
+    void updateEntityFromRequest(AmenityRequestDTO amenityRequestDTO,@MappingTarget Image image);
 }
