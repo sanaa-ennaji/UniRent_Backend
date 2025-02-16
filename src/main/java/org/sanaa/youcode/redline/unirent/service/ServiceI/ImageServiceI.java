5fa2bb932b2b51f5b53460076f523a53c5c50d23
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 22:45:59 +0100

    update on  22:45:59,58

--- src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/ImageServiceI.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/service/ServiceI/ImageServiceI.java
@@ -1,4 +1,14 @@
 package org.sanaa.youcode.redline.unirent.service.ServiceI;
 
+import org.sanaa.youcode.redline.unirent.model.dto.Request.ImageRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.ImageResponseDTO;
+
+import java.util.List;
+
 public interface ImageServiceI {
+    ImageResponseDTO getImageById(Long id);
+    List<ImageResponseDTO> getAllImages();
+    ImageResponseDTO uploadImage(ImageRequestDTO requestDTO);
+    ImageResponseDTO updateImage(Long id, ImageRequestDTO requestDTO);
+    void deleteImage(Long id);
 }
