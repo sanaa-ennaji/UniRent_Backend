1c80378c6fda668217b8105927623c94d45b51ef
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-16 23:17:51 +0100

    update on  23:17:51,33

--- src/main/java/org/sanaa/youcode/redline/unirent/controller/BookingController.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/BookingController.java
@@ -1,4 +1,45 @@
 package org.sanaa.youcode.redline.unirent.controller;
 
+import org.sanaa.youcode.redline.unirent.model.dto.Request.BookingRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.BookingResponseDTO;
+import org.sanaa.youcode.redline.unirent.service.BookingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+RestController
+@RequestMapping("/api/bookings")
 public class BookingController {
-}
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookingResponseDTO> getBookingById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookingService.getBookingById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookingResponseDTO>> getAllBookings() {
+        return ResponseEntity.ok(bookingService.getAllBookings());
+    }
+
+    @PostMapping
+    public ResponseEntity<BookingResponseDTO> createBooking(@RequestBody BookingRequestDTO requestDTO) {
+        return ResponseEntity.ok(bookingService.createBooking(requestDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookingResponseDTO> updateBooking(
+        @PathVariable Long id, @RequestBody BookingRequestDTO requestDTO) {
+        return ResponseEntity.ok(bookingService.updateBooking(id, requestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBooking(@PathVariable Long id) {
+        bookingService.deleteBooking(id);
+        return ResponseEntity.noContent().build();
+    }
