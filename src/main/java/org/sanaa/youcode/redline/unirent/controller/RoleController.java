b655fb3f4524de33224b61eeb947ad570d56295c
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 17:04:34 +0100

    update on  17:04:34,26

--- src/main/java/org/sanaa/youcode/redline/unirent/controller/RoleController.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/RoleController.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+
+
 @Validated
 @RestController
 @RequestMapping("/api/roles")
