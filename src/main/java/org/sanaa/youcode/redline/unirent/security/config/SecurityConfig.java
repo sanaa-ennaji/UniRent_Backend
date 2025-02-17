643e05a24f6e809640801bbef76a7fd4ba23bbfc
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 14:57:52 +0100

    update on  14:57:52,31

--- src/main/java/org/sanaa/youcode/redline/unirent/security/config/SecurityConfig.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/security/config/SecurityConfig.java
@@ -1,4 +1,24 @@
 package org.sanaa.youcode.redline.unirent.security.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .dispatcherTypeMatchers("/api/roles/**").permitAll()
+            .dispatcherTypeMatchers(HttpMethod.GET, "/api/**").permitAll()
+            .dispatcherTypeMatchers(HttpMethod.POST, "/api/**").permitAll()
+            .dispatcherTypeMatchers(HttpMethod.PUT, "/api/**").permitAll()
+            .dispatcherTypeMatchers(HttpMethod.DELETE, "/api/**").permitAll()
+            .anyRequest().authenticated();
+    }
 }
