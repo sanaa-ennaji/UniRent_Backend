66c6579407aa86110c3f86d4185fa80b6cbd2131
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 15:01:56 +0100

    update on  15:01:56,22

--- src/main/java/org/sanaa/youcode/redline/unirent/security/config/SecurityConfig.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/security/config/SecurityConfig.java
@@ -14,15 +14,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/api/roles/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/api/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/api/**").permitAll()
-            .antMatchers(HttpMethod.PUT, "/api/**").permitAll()
-            .antMatchers(HttpMethod.DELETE, "/api/**").permitAll()
+            .csrf().disable() 
+            .authorizeHttpRequests(authz -> authz
+            .requestMatchers("/api/roles/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
+            .requestMatchers(HttpMethod.PUT, "/api/**").permitAll()
+            .requestMatchers(HttpMethod.DELETE, "/api/**").permitAll()
             .anyRequest().authenticated();
         return http.build();
     }
 }
+
 
