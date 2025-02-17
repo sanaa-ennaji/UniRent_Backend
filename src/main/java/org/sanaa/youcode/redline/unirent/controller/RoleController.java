a6c6939464e24b39e011c07f0ba9a8ee35b9ddcb
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 09:07:39 +0100

    update on   9:07:39,19

--- src/main/java/org/sanaa/youcode/redline/unirent/controller/RoleController.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/RoleController.java
@@ -1,4 +1,48 @@
 package org.sanaa.youcode.redline.unirent.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.RoleRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.RoleResponseDTO;
+import org.sanaa.youcode.redline.unirent.service.RoleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/roles")
 public class RoleController {
+    private final RoleService roleService;
+
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoleResponseDTO> getRoleById(@PathVariable Long id) {
+        return ResponseEntity.ok(roleService.getRoleById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleResponseDTO>> getAllRoles() {
+        return ResponseEntity.ok(roleService.getAllRoles());
+    }
+
+    @PostMapping
+    public ResponseEntity<RoleResponseDTO> createRole(@RequestBody RoleRequestDTO requestDTO) {
+        return ResponseEntity.ok(roleService.createRole(requestDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RoleResponseDTO> updateRole(
+        @PathVariable Long id, @RequestBody RoleRequestDTO requestDTO) {
+        return ResponseEntity.ok(roleService.updateRole(id, requestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRole(@PathVariable Long id) {
+        roleService.deleteRole(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
