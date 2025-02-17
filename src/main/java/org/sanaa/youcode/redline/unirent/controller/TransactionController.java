083deead5afa665c2a2c78e069ffa57d9432a4b9
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 09:11:43 +0100

    update on   9:11:43,21

--- src/main/java/org/sanaa/youcode/redline/unirent/controller/TransactionController.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/TransactionController.java
@@ -1,4 +1,47 @@
 package org.sanaa.youcode.redline.unirent.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.TransactionRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.TransactionResponseDTO;
+import org.sanaa.youcode.redline.unirent.service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionResponseDTO> getTransactionById(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.getTransactionById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponseDTO>> getAllTransactions() {
+        return ResponseEntity.ok(transactionService.getAllTransactions());
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionResponseDTO> createTransaction(@RequestBody TransactionRequestDTO requestDTO) {
+        return ResponseEntity.ok(transactionService.createTransaction(requestDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TransactionResponseDTO> updateTransaction(
+        @PathVariable Long id, @RequestBody TransactionRequestDTO requestDTO) {
+        return ResponseEntity.ok(transactionService.updateTransaction(id, requestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
+        transactionService.deleteTransaction(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/org/sanaa/youcode/redline/unirent/controller/UserController.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/UserController.java
@@ -1,5 +1,53 @@
 package org.sanaa.youcode.redline.unirent.controller;
 
-public class UserController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO requestDTO) {
+        return ResponseEntity.ok(userService.createUser(requestDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @RequestBody UserRequestDTO requestDTO) {
+        return ResponseEntity.ok(userService.updateUser(id, requestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponseDTO> loginUser(@RequestBody LoginRequestDTO loginRequestDTO) {
+        return ResponseEntity.ok(userService.loginUser(loginRequestDTO));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserRequestDTO requestDTO) {
+        return ResponseEntity.ok(userService.registerUser(requestDTO));
+    }
 }
+
