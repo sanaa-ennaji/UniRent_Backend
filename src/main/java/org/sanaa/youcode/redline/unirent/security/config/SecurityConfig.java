bfc702e2be145130b20d2aa38324e524798bc203
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 14:59:54 +0100

    update on  14:59:54,16

--- src/main/java/org/sanaa/youcode/redline/unirent/security/config/SecurityConfig.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/security/config/SecurityConfig.java
@@ -1,24 +1,28 @@
 package org.sanaa.youcode.redline.unirent.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .authorizeRequests()
-            .dispatcherTypeMatchers("/api/roles/**").permitAll()
-            .dispatcherTypeMatchers(HttpMethod.GET, "/api/**").permitAll()
-            .dispatcherTypeMatchers(HttpMethod.POST, "/api/**").permitAll()
-            .dispatcherTypeMatchers(HttpMethod.PUT, "/api/**").permitAll()
-            .dispatcherTypeMatchers(HttpMethod.DELETE, "/api/**").permitAll()
+            .antMatchers("/api/roles/**").permitAll()
+            .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/**").permitAll()
+            .antMatchers(HttpMethod.PUT, "/api/**").permitAll()
+            .antMatchers(HttpMethod.DELETE, "/api/**").permitAll()
             .anyRequest().authenticated();
+        return http.build();
     }
 }
+
