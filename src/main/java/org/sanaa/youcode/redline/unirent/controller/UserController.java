06cd42ae44c0b08a034de9aa7032b328b59053c2
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 17:32:14 +0100

    update on  17:32:14,19

--- src/main/java/org/sanaa/youcode/redline/unirent/controller/UserController.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Validated
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
