4f0bc955df0f222d14b0c61c8fcca90dfa6b31ad
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 11:50:32 +0100

    update on  11:50:32,26

--- src/main/java/org/sanaa/youcode/redline/unirent/controller/RoleController.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/RoleController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @Validated
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/roles")
 public class RoleController {
     private final RoleService roleService;
 
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
 
 
     @GetMapping("/{id}")
