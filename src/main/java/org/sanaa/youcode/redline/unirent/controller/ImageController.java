12f360384acaf4754d690351babf7cab52f18cef
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-17 09:03:34 +0100

    update on   9:03:34,19

---
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/ImageController.java
@@ -0,0 +1,47 @@
+package org.sanaa.youcode.redline.unirent.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.ImageRequestDTO;
+import org.sanaa.youcode.redline.unirent.model.dto.Response.ImageResponseDTO;
+import org.sanaa.youcode.redline.unirent.service.ImageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Validated
+@RequiredArgsConstructor
+@RequestMapping("/api/images")
+public class ImageController {
+    private final ImageService imageService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ImageResponseDTO> getImageById(@PathVariable Long id) {
+        return ResponseEntity.ok(imageService.getImageById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ImageResponseDTO>> getAllImages() {
+        return ResponseEntity.ok(imageService.getAllImages());
+    }
+
+    @PostMapping
+    public ResponseEntity<ImageResponseDTO> createImage(@RequestBody ImageRequestDTO requestDTO) {
+        return ResponseEntity.ok(imageService.uploadImage(requestDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ImageResponseDTO> updateImage(
+        @PathVariable Long id, @RequestBody ImageRequestDTO requestDTO) {
+        return ResponseEntity.ok(imageService.updateImage(id, requestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteImage(@PathVariable Long id) {
+        imageService.deleteImage(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
