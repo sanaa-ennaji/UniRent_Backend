7a02affd245fd57990c1d3ecb6daef9b1cee9c8d
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-19 11:38:09 +0100

    update on  11:38:09,20

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/PropertyMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/PropertyMapper.java
@@ -1,6 +1,7 @@
 package org.sanaa.youcode.redline.unirent.model.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.sanaa.youcode.redline.unirent.model.dto.Request.PropertyRequestDTO;
 import org.sanaa.youcode.redline.unirent.model.dto.Response.PropertyResponseDTO;
@@ -9,7 +10,9 @@ import org.sanaa.youcode.redline.unirent.model.entity.Property;
 import java.util.List;
 @Mapper(componentModel = "spring" )
 public interface PropertyMapper {
+    @Mapping(source = "user.id", target = "landlordId")
     PropertyResponseDTO toResponseDTO(Property entity) ;
+    
     Property toEntity(PropertyRequestDTO requestDTO);
     List<PropertyResponseDTO> toResponseDTOList (List<Property> entities);
     List<Property> toEntityList(List<PropertyRequestDTO> requestDTOs);
