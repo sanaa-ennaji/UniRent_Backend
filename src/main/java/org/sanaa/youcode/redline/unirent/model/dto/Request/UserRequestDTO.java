a8d9bed78f139e387b8fcc70c7eeb5e1ce0fc841
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-21 10:19:02 +0100

    update on  10:19:02,19

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/UserRequestDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/UserRequestDTO.java
@@ -2,6 +2,7 @@ package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequestDTO {
-    @NotBlank
+    @NotBlank(message = "Name cannot be blank")
     private String name;
 
-    @Email
-    @NotBlank
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
     private String password;
 
     private String phoneNumber;
