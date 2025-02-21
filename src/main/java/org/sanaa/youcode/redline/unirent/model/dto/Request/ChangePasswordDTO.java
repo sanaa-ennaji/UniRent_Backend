0fc78eac46c11257613145ebdb7396d1f42a0eb0
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-21 12:16:10 +0100

    update on  12:16:10,25

--- src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/ChangePasswordDTO.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/ChangePasswordDTO.java
@@ -1,4 +1,19 @@
 package org.sanaa.youcode.redline.unirent.model.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChangePasswordDTO {
+
+    @NotBlank
+    private String oldPassword;
+
+    @NotBlank
+    private String newPassword;
 }
+
