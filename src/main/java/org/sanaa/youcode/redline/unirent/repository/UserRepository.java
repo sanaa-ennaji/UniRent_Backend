a0d3f38be9e09f2dad508ec38ce5d46dad78fec4
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-25 14:37:54 +0000

    update on  14:37:54,21

--- src/main/java/org/sanaa/youcode/redline/unirent/repository/UserRepository.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     boolean existsByEmail(String email);
-    Optional<AppUser> findByUsername(String name);
+    Optional<AppUser> findByName(String name);
 }
