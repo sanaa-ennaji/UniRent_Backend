8f6852f1e5709dbf89af08019f929066cf79447f
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-10 12:57:34 +0000

    making the edge to chage the config

--- src/main/java/org/sanaa/youcode/redline/unirent/security/config/JwtUtil.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/security/config/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String SECRET_KEY = "secret";
+    private String SECRET_KEY = "${jwt.secret}";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
