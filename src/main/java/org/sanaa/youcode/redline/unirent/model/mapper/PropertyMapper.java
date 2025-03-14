e16e4660e959df21df08d66b4bf78fec58e8b3f9
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-14 14:35:38 +0000

    fixed the insertion on the properties

--- src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/PropertyMapper.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/mapper/PropertyMapper.java
@@ -3,14 +3,32 @@ package org.sanaa.youcode.redline.unirent.model.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.sanaa.youcode.redline.unirent.model.dto.Request.PropertyRequestDTO;
 import org.sanaa.youcode.redline.unirent.model.dto.Response.PropertyResponseDTO;
 import org.sanaa.youcode.redline.unirent.model.entity.Property;
+import org.sanaa.youcode.redline.unirent.model.entity.University;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Mapper(componentModel = "spring" )
 public interface PropertyMapper {
-    PropertyResponseDTO toResponseDTO(Property entity) ;
+    @Mapping(target = "landlordId", source = "landlord.id")
+    @Mapping(target = "universityIds", source = "universities", qualifiedByName = "mapUniversitiesToIds")
+    PropertyResponseDTO toResponseDTO(Property entity);
+
+
+    @Named("mapUniversitiesToIds")
+    default List<Long> mapUniversitiesToIds(List<University> universities) {
+        if (universities == null) {
+            return Collections.emptyList();
+        }
+        return universities.stream()
+            .map(University::getId)
+            .collect(Collectors.toList());
+    }
     Property toEntity(PropertyRequestDTO requestDTO);
     List<PropertyResponseDTO> toResponseDTOList (List<Property> entities);
     List<Property> toEntityList(List<PropertyRequestDTO> requestDTOs);
