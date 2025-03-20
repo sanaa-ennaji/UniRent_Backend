3f9d739c3121269c8d6c182cf3e7a7cb6910a001
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-03-20 19:44:33 +0000

    add secret key

--- src/main/java/org/sanaa/youcode/redline/unirent/controller/PaymentController.java
+++ src/main/java/org/sanaa/youcode/redline/unirent/controller/PaymentController.java
@@ -1,4 +1,34 @@
 package org.sanaa.youcode.redline.unirent.controller;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.sanaa.youcode.redline.unirent.model.dto.Request.PaymentRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payments")
 public class PaymentController {
+
+    @Value("${stripe.secret-key}")
+    private String stripeSecretKey;
+
+    @PostMapping("/create-payment-intent")
+    public String createPaymentIntent(@RequestBody PaymentRequest paymentRequest) throws StripeException {
+        Stripe.apiKey = stripeSecretKey;
+
+        // Create a PaymentIntent with the order amount and currency
+        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
+            .setAmount(paymentRequest.getAmount() * 100L) // Convert to cents
+            .setCurrency("usd") // Change to your currency
+            .setDescription("Payment for property booking")
+            .build();
+
+        PaymentIntent paymentIntent = PaymentIntent.create(params);
+
+        // Return the client secret to the frontend
+        return paymentIntent.getClientSecret();
+    }
 }
---
+++ src/main/java/org/sanaa/youcode/redline/unirent/model/dto/Request/PaymentRequest.java
@@ -0,0 +1,13 @@
+package org.sanaa.youcode.redline.unirent.model.dto.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequest {
+    private Long amount;
+    private String currency;
+}
